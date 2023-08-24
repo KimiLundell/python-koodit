2a17db75cbd68600bba2d4d3e765c3c009b3009f
Kimi <kimilu@metropolia.fi>
2023-08-24 14:55:38 +0300

    Initial commit

---
+++ tehtava2/tehtava2.4.py
@@ -0,0 +1,7 @@
+luku1 = float(input("Kerro ensimmäinen luku: "))
+luku2 = float(input("Kerro toinen luku: "))
+luku3 = float(input("Kerro kolmas luku: "))
+
+print("Lukujesi summa on: " + str(luku1 + luku2 + luku3))
+print("Lukujesi tulo on: " + str(luku1 * luku2 * luku3))
+print("Lukujesi keskiarvo on: " + (str(luku1 + luku2 + luku3) * 3))
