0d5165b8930362b51bcfa0c1e4b314a3a2d9b6c3
Kimi <kimilu@metropolia.fi>
2023-08-24 19:25:07 +0300

    Initial commit

---
+++ tehtava2/tehtava2.6.py
@@ -0,0 +1,6 @@
+import random
+print("Tässä sinulle koodeja!")
+print(str(random.randint(0,9)) + str(random.randint(0,9)) + str(random.randint(0,9)))
+
+print(str(random.randint(1,6)) + str(random.randint(1,6)) + str(random.randint(1,6)) + str(random.randint(1,6)))
+#Pitkä teksti
