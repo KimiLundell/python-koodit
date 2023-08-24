d7c5e7ba3a69b6453c46ba307333abc1ef97d346
Kimi <kimilu@metropolia.fi>
2023-08-24 14:32:42 +0300

    Initial commit

---
+++ tehtava2/tehtava2.3.py
@@ -0,0 +1,6 @@
+import math
+kanta = float(input("Mikä on suorakulmiosi kanta?"))
+korkeus = float(input("Mikä on suorakulmiosi korkeus?"))
+
+print("Suorakulmiosi piiri on: " + str(kanta * 2 + korkeus * 2))
+print("Suorakulmiosi pinta-ala on: " + str(kanta * korkeus))
