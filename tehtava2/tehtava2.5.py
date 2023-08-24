87f6defa237addf76cd5620611c70d75dd6f4f32
Kimi <kimilu@metropolia.fi>
2023-08-24 15:36:18 +0300

    Initial commit

---
+++ tehtava2/tehtava2.5.py
@@ -0,0 +1,10 @@
+leiviska = float(input("Anna leiviskät."))
+naula = float(input("Anna naulat."))
+luoti = float(input("Anna luodit."))
+
+leiviskaNaul = leiviska * 20
+naulaLuot = naula * 32
+luotiGram = 13.3
+
+leiviskaLuot = leiviskaNaul * 32
+
