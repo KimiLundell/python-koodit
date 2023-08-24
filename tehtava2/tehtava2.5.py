175847bfde02e9156d9bcf79ac673fa961b05778
Kimi <kimilu@metropolia.fi>
2023-08-24 18:13:56 +0300

    Korjattu grammojen laskenta

--- tehtava2/tehtava2.5.py
+++ tehtava2/tehtava2.5.py
@@ -2,9 +2,12 @@ leiviska = float(input("Anna leiviskät."))
 naula = float(input("Anna naulat."))
 luoti = float(input("Anna luodit."))
 
-leiviskaNaul = leiviska * 20
-naulaLuot = naula * 32
-luotiGram = 13.3
+leiviskaNauloiksi = leiviska * 20
+naulaLuodeiksi = naula * 32
+luotiGramma = luoti * 13.3
 
-leiviskaLuot = leiviskaNaul * 32
-
+leiviskaLuodeiksi = leiviskaNauloiksi * 32
+leiviskaGramma = leiviskaLuodeiksi * 13.3
+naulaGramma = naulaLuodeiksi * 13.3
+grammat = luotiGramma + naulaGramma + leiviskaGramma
+print(grammat)
