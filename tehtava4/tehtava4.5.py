ea3b62ce9ef8de1153e7cb99962dfff4292a1404
Kimi <kimilu@metropolia.fi>
2023-08-31 16:27:12 +0300

    Initial commit

---
+++ tehtava4/tehtava4.5.py
@@ -0,0 +1,14 @@
+tunnus1 = input("Kerro käyttäjätunnus: ")
+sana1 = input("Kerro salasana: ")
+tunnus = "python"
+sana = "rules"
+kerrat = 1
+while tunnus1!=tunnus and sana1!=sana:
+    kerrat = kerrat + 1
+    tunnus1 = input("Kerro käyttäjätunnus: ")
+    sana1 = input("Kerro salasana: ")
+    if kerrat==5:
+        print("Pääsy evätty")
+        break
+else:
+    print("Tervetuloa")
