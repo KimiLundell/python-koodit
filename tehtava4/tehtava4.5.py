9fde419b76f8267cb0d74f0afd0558ce6a692b4b
Kimi <kimilu@metropolia.fi>
2023-09-04 09:14:19 +0300

    Initial commit

--- tehtava4/tehtava4.5.py
+++ tehtava4/tehtava4.5.py
@@ -3,7 +3,7 @@ sana1 = input("Kerro salasana: ")
 tunnus = "python"
 sana = "rules"
 kerrat = 1
-while tunnus1!=tunnus and sana1!=sana:
+while tunnus1!=tunnus or sana1!=sana:
     kerrat = kerrat + 1
     tunnus1 = input("Kerro käyttäjätunnus: ")
     sana1 = input("Kerro salasana: ")
