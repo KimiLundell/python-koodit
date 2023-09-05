3e2af879b0c3ac180ba1fd47074ae42f8ec6eb57
Kimi <kimilu@metropolia.fi>
2023-09-05 13:12:15 +0300

    Initial commit

--- tehtava5/tehtava5.4.py
+++ tehtava5/tehtava5.4.py
@@ -2,9 +2,5 @@ lista = []
 for s in range(5):
     syöte = input("Syötä kaupungin nimi: ")
     lista.append(syöte)
-for s in range(1):
-    print(lista[0])
-    print(lista[1])
-    print(lista[2])
-    print(lista[3])
-    print(lista[4])
+for nimi in lista:
+    print(nimi)
