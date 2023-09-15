0e9f67243b359dd6fec46475c621f7b4326f0a7a
Kimi <kimilu@metropolia.fi>
2023-09-15 13:35:19 +0300

    Initial commit

---
+++ tehtava8/tehtava8.1.py
@@ -0,0 +1,23 @@
+import mysql.connector
+
+def testi(koodi):
+    sql = "select name, municipality from airport"
+    sql += " where ident='" + koodi + "'"
+    print(sql)
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    if kursori.rowcount >0:
+        for rivi in tulos:
+            print(rivi)
+    return
+yhteys = mysql.connector.connect(
+    host="localhost",
+    port=3306,
+    database="flight_game",
+    user="root",
+    password="Servermaster20!",
+    autocommit=True)
+
+koodi = input("Syötä lentoaseman ICAO-koodi: ")
+testi(koodi)
